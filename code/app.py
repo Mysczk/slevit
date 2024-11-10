346ceb484b5cc201307fb860c0d10866446732f6
Mysczk <michal.soukup.jr@gmail.com>
2024-11-10 12:18:33 +0100

    structure setup

--- code/app.py
+++ code/app.py
@@ -1,14 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from redis import Redis
+
+redis = Redis(host='redis',port=6379,decode_responses=True)
 
 app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 @app.route("/index")
 def index():
-    return "<h1>Já jsem webovka s nadpisem</h1>"
+    if(redis.exists("g")):
+        data = redis.get("Cached:g")
+        return render_template("index.html", message=data)
+    else:
+        redis.set("Cached:g", 'bagr', 10)
+        return render_template("index.html", message="neexistuje")
 
-@app.route("/datasets")
-def datasets():
-    return "<h1>Já jsem jiná webovka s nadpisem</h1><p>mám i odstavec</p>"
+
+@app.route("/trending")
+def trending():
+    listik = []
+    redis.set("Cached:a",1, 6)
+    redis.set("Cached:b",2, 6)
+    redis.set("Cached:c",3, 6)
+    for key in redis.scan_iter("Cache:*"):
+        listik.append(key)
+    return render_template("trending.html", pages = listik)
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")  
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
